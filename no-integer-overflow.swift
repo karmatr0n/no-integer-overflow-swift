fca8cf31922889860d36fa6120f789f2f0e0d9f4
Alejandro Juárez <juarlex@gmail.com>
2014-12-11 16:56:59 -0600

    Fixing a minor typo

--- no-integer-overflow.swift
+++ no-integer-overflow.swift
@@ -7,6 +7,6 @@ a += 1 // Attempting to convert a into -2147483648 using the most significant bi
 // We had bad news because the integer overflow cannot be used for security exploitation in Swift.
 // We just will get a Runtime error
 if (a < 0 ) {
-      println("Interger overflow..")
+      println("Integer overflow..")
 }
 
